1ae5b5c4361637d73667f6b03fd8e6032e5f289a
midoelmelegy <midoelmelegy@gmail.com>
2023-03-21 21:16:47 +0800

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -42,8 +42,6 @@ export default function Header({ photo }: { photo?: string }) {
         <a
           className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-blue-600 text-white px-5 py-2 text-sm shadow-md hover:bg-blue-500 bg-blue-600 font-medium transition"
           href="/dream"
-          target="_blank"
-          rel="noopener noreferrer"
         >
           <Github />
           <p>Generate</p>
